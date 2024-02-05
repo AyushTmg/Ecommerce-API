5914cd6012433a0ce15e9f59bd74925e2b3c56e8
AyushTmg <ayushtmg20040722@gmail.com>
2024-02-05 18:52:58 +0545

    Refactor: Code refactored and added custom action for normal users for getting thier activities in the application  and also updating their profile

--- user_activity_logs/views.py
+++ user_activity_logs/views.py
@@ -1,33 +1,29 @@
-from .serializers import UserActivitySerializer
-from rest_framework.viewsets import ModelViewSet
 from authentication.models import User 
+from .serializers import UserActivitySerializer
+
+
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser,IsAuthenticated
+from rest_framework.status  import HTTP_200_OK
 
 
 
 
 # ! User Activity ViewSet 
-class UserActvityViewSet(ModelViewSet):
+class UserActvityViewSet(ModelViewSet):# Create your models here.
+
     http_method_names=['get','head','options','put','patch','delete']
+    permission_classes=[IsAdminUser]
         
 
     def get_queryset(self):
         """
-        Method to over ride the queryset ro return user activity
-        based on logged in user roles 
+        Method to over ride the queryset to return user activity
+        of all the user in the database for admin user 
         """
 
-        # ! This runs if the user is admin or a staff member
-        if (self.request.user.is_staff or self.request.user.is_superuser):
-            return User.objects.all().prefetch_related(
-                'order',
-                'order__order_item',
-                'order__order_item__product',
-                'order__order_item__product__product_image',
-                'review'
-                )
-        
-
-        # ! else  this is executed when user is a normal user 
         return User.objects.filter(id=self.request.user.id).prefetch_related(
                 'order',
                 'order__order_item',
@@ -36,8 +32,45 @@ class UserActvityViewSet(ModelViewSet):
                 'review'
                 )
     
-        
+    # ! Serailizer Class 
     serializer_class=UserActivitySerializer
+    
+
+    # !Custom Action for normal user's 
+    @action(detail = False,methods=['GET','PUT'], permission_classes=[IsAuthenticated])
+    def me(self,request):
+        """
+        Adding custom me action in the viewset to show the user activity
+        of the logged in normal users 
+        """
+
+        user=self.request.user
+        # ! Viewing the normal user activty logs 
+        if self.request.method=='GET':
+                user=User.objects.filter(id=user.id).prefetch_related(
+                    'order',
+                    'order__order_item',
+                    'order__order_item__product',
+                    'order__order_item__product__product_image',
+                    'review'
+                    )
+                serializer=self.serializer_class(user,many=True)
+                return Response(serializer.data,status=HTTP_200_OK)
+        
+
+        # ! Taking the data from the request and updating the noraml user instance
+        if self.request.method=='PUT':
+            serializer=self.serializer_class(user,data=request.data)
+            serializer.is_valid()
+            serializer.save()
+            return Response(serializer.data,status=HTTP_200_OK)
+        
+
+
+    
+
+    
+        
 
     
        
