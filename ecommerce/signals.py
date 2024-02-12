e7cd04b8e4fe73b800eb21dc6dc189f8b1f14c7d
AyushTmg <ayushtmg20040722@gmail.com>
2024-02-12 14:52:02 +0545

    Feat: Added a signal that creates a cart object of registered user  after the user is registered in the application

--- ecommerce/signals.py
+++ ecommerce/signals.py
@@ -1,8 +1,17 @@
-from django.dispatch import Signal,receiver
+from .models import Cart
 from .tasks import send_order_email_confirmation_task
+
+
+from django.dispatch import Signal,receiver
+from django.conf   import settings
+from django.db.models import signals
+
+
+# ! Custom Signal 
 order_created=Signal()
 
 
+# ! Signal Used when a Order is created 
 @receiver(order_created)
 def on_order(sender,**kwargs):
     """
@@ -18,7 +27,7 @@ def on_order(sender,**kwargs):
         
     # ! This data is to inform supplier about the order has been
     # ! placed and is assumed that all the product are sold by a
-    # ! single supplier and the supplier email should be used in this
+    # ! single supplier 
     data_for_sending_mail_to_suppliser={
         'user':str(kwargs['order'].user),
         'subject':"Order Received"
@@ -31,3 +40,13 @@ def on_order(sender,**kwargs):
     )
 
 
+
+#! Signals Used when a user registers a account
+@receiver(signals.post_save,sender=settings.AUTH_USER_MODEL)
+def create_user_profile(sender,**kwargs):
+    """
+    Whenever a User instance is save this function is 
+    executed to create a associated cart for the user 
+    """
+    if kwargs['created']:
+         Cart.objects.create(user=kwargs['instance'])
